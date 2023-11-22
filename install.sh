b119520f2da68aa72ada29f64d52215b88443b3c
João <145498965+joao406@users.noreply.github.com>
2023-11-22 03:40:17 -0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+echo "[*] CHECKING ESSENTIAL PACKAGES..."
+[[ $(type -P tar) ]] && echo "[+] TAR INSTALLED" || { echo "[-] Install tar to continue!" ; exit 1 ;}
+[[ $(type -P emacs) ]] && echo "[+] EMACS INSTALLED" || { echo "[-] Install emacs to continue!" ; exit 1 ;}
+
+echo -e "\n[*] INSTALLING CONFIG FILE..."
+
 tar -xf emacs_config.tar \
 	&& cp -r .emacs.d ~/ \
 	&& echo "[+] Sucessfully instaled!" \
